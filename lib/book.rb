3af2952450972572a61f9dbd3087579649e14ef3
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-10 14:00:43 -0500

    commit tree 091284b92de30327d20c2e3d6a5734d532cbbfb9

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+require 'pry'
+
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        # @author = author
+        # @page_count = page_count
+        # @genre = genre
+  
+    end
+
+    def turn_page
+       puts @turn_page = "Flipping the page...wow, you read fast!"
+    end
+
+
+
+end
+
+# book1 = Book.new("HP1","JKR",252,"fiction")
+# puts book1
+
+# binding.pry
+
+
 
 
